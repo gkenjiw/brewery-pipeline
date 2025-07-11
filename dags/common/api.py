dca9fc7bb4915770379a0c94bff956a13701ae57
Kenji Watanabe <gabrielkenji.watanabe@gmail.com>
2025-07-11 05:32:37 -0300

    Added error handling with exception, also logging added to track

--- dags/common/api.py
+++ dags/common/api.py
@@ -1,21 +1,39 @@
 
 import requests
 import json
-import os
+import logging
 from datetime import datetime, timezone
 from pathlib import Path
+from airflow.exceptions import AirflowException
 
 # See https://www.openbrewerydb.org/documentation
+logger = logging.getLogger(__name__)
+
 def fetch_brewery_data():
     url = "https://api.openbrewerydb.org/v1/breweries"
-    response = requests.get(url)
-    response.raise_for_status()
-    data = response.json()
 
-    timestamp = datetime.now(timezone.utc).strftime("%Y%m%d%H%M%S")
-    output_dir = Path("/opt/airflow/data/bronze")
-    output_dir.mkdir(parents=True, exist_ok=True)
+    try:
+        logger.info("Requesting data from Brewery DB API...")
+        response = requests.get(url, timeout=10)
+        response.raise_for_status()
+        data = response.json()
 
-    output_path = output_dir / f"breweries_{timestamp}.json"
-    with open(output_path, "w") as f:
-        json.dump(data, f)
+        if not data:
+            raise AirflowException("API returned no data.")
+
+        timestamp = datetime.now(timezone.utc).strftime("%Y%m%d%H%M%S")
+        output_dir = Path("/opt/airflow/data/bronze")
+        output_dir.mkdir(parents=True, exist_ok=True)
+        output_path = output_dir / f"breweries_{timestamp}.json"
+
+        with open(output_path, "w") as f:
+            json.dump(data, f)
+        
+        logger.info(f"Saved raw data to {output_path}")
+        
+    except requests.exceptions.RequestException as e:
+        logger.exception("API request failed")
+        raise AirflowException(f"API error: {str(e)}")
+    except Exception as e:
+        logger.exception("Unknown error occurred")
+        raise AirflowException(f"Unexpected error: {str(e)}")
--- dags/common/transforms.py
+++ dags/common/transforms.py
@@ -1,31 +1,62 @@
 import pandas as pd
 import json
+import logging
 from pathlib import Path
+from airflow.exceptions import AirflowException
+
+logger = logging.getLogger(__name__)
 
 def transform_to_silver():
-    # Load latest JSON file from bronze
-    bronze_dir = Path("/opt/airflow/data/bronze")
-    latest_file = sorted(bronze_dir.glob("*.json"))[-1]
-    with open(latest_file) as f:
-        data = json.load(f)
-    df = pd.DataFrame(data)
+    try:
+        # Load latest JSON file from bronze
+        bronze_dir = Path("/opt/airflow/data/bronze")
+        latest_file = sorted(bronze_dir.glob("*.json"))[-1]
+        if not latest_file:
+            raise AirflowException("No raw files found in bronze layer.")
+        
+        with open(latest_file) as f:
+            logger.info(f"Reading file {f} in bronze layer")
+            data = json.load(f)
+            
+        df = pd.DataFrame(data)
 
-    # Transform and partition by state
-    df = df[df['state'].notnull()]
-    df['state'] = df['state'].fillna('unknown')
+        # Transform and partition by state
+        logger.info("Transforming file to silver layer")
+        df = df[df['state'].notnull()]
+        df['state'] = df['state'].fillna('unknown')
 
-    silver_dir = Path("/opt/airflow/data/silver")
-    silver_dir.mkdir(parents=True, exist_ok=True)
-    for state, group in df.groupby("state"):
-        group.to_parquet(silver_dir / f"breweries_{state}.parquet", index=False)
+        silver_dir = Path("/opt/airflow/data/silver")
+        silver_dir.mkdir(parents=True, exist_ok=True)
+        for state, group in df.groupby("state"):
+            group.to_parquet(silver_dir / f"breweries_{state}.parquet", index=False)
+
+        logger.info("Transformed file saved in silver folder")
+
+    except Exception as e:
+        logger.exception("Failed to transform data to silver layer")
+        raise AirflowException(f"Silver transformation failed: {e}")
 
 def aggregate_gold():
-    silver_dir = Path("/opt/airflow/data/silver")
-    dfs = [pd.read_parquet(p) for p in silver_dir.glob("*.parquet")]
-    full_df = pd.concat(dfs)
+    try:
+        silver_dir = Path("/opt/airflow/data/silver")
+        logger.info("Reading files in silver layer")
+        dfs = [pd.read_parquet(p) for p in silver_dir.glob("*.parquet")]
+        if not dfs:
+            raise AirflowException("No silver files found to aggregate.")
+        
+        full_df = pd.concat(dfs)
 
-    agg_df = full_df.groupby(["state", "brewery_type"]).size().reset_index(name="count")
-    gold_dir = Path("/opt/airflow/data/gold")
-    gold_dir.mkdir(parents=True, exist_ok=True)
+        if full_df.empty:
+            raise AirflowException("No data to aggregate in silver layer.")
+        
+        logger.info("Aggregating data to gold layer")
+        agg_df = full_df.groupby(["state", "brewery_type"]).size().reset_index(name="count")
+        gold_dir = Path("/opt/airflow/data/gold")
+        gold_dir.mkdir(parents=True, exist_ok=True)
 
-    agg_df.to_csv(gold_dir / f"breweries_aggregated.csv", index=False)
+        agg_df.to_csv(gold_dir / f"breweries_aggregated.csv", index=False)
+        logger.info("Aggregated data saved in gold folder")
+
+    except Exception as e:
+        logger.exception("Failed to aggregate data to gold layer")
+        raise AirflowException(f"Gold aggregation failed: {e}")
