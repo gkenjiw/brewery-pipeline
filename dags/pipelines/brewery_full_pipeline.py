ea77bdceaf51de9b25f47c697feb52b210c618b9
Kenji Watanabe <gabrielkenji.watanabe@gmail.com>
2025-07-11 14:05:12 -0300

    name fix

--- dags/pipelines/brewery_full_pipeline.py
+++ dags/pipelines/brewery_full_pipeline.py
@@ -5,7 +5,7 @@ from common.api import fetch_brewery_data
 from common.transforms import transform_to_silver, aggregate_gold
 from common.notifiers import notify_discord_failure, notify_discord_success
 
-with DAG("brewery_pipeline_all_layers", start_date=datetime(2025,7,1), schedule="@daily") as dag:
+with DAG("brewery_full_pipeline", start_date=datetime(2025,7,1), schedule="@daily") as dag:
     
     bronze_task = PythonOperator(
         task_id="bronze_ingestion",
